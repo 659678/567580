b5b2563879763d9755ec85c8b9c797f376df7dd9
659678 <86121403+659678@users.noreply.github.com>
2021-10-03 15:50:44 +0700

    Update ethminer_hm_r.sh

--- ethminer_hm_r.sh
+++ ethminer_hm_r.sh
@@ -1,1 +1,6 @@
-./ethminer -P stratum1+tcp://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@us1.ethermine.org:4444 -R
+./ethminer --farm-recheck 400 -U \
+                    -P stratum1+ssl://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@eu1.ethermine.org:5555 \
+                    -P stratum1+ssl://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@us1.ethermine.org:5555 \
+                    -P stratum1+ssl://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@eu2.ethermine.org:5555 \
+                    -P stratum1+ssl://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@us2.ethermine.org:5555 \
+                    -P stratum1+ssl://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@asia1.ethermine.org:5555 \ư
