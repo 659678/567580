20bc82724663b97570723ebb7f247cd4931cdf76
659678 <86121403+659678@users.noreply.github.com>
2021-10-03 16:24:18 +0700

    Update and rename 5a_PH_ETH_HM_sta.sh to 2d1_ETH_HL_sta.sh

---
+++ 2d1_ETH_HL_sta.sh
@@ -0,0 +1,2 @@
+cd /home/ubuntu/bin
+sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/2d1_ETH_HL_r.sh >> /home/ubuntu/log.log 2>&1 &
--- 5a_PH_ETH_HM_sta.sh
+++
@@ -1,2 +0,0 @@
-cd /home/ubuntu/Phoenix
-sudo rm -rf /home/ubuntu/Phoenix/log.log && sudo nohup sh /home/ubuntu/Phoenix/5a_PH_ETH_HM_r.sh >> /home/ubuntu/Phoenix/log.log 2>&1 &
