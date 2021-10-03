d3553b42fdbb9d64241d7fa8de51a76482e702cb
659678 <86121403+659678@users.noreply.github.com>
2021-10-03 16:21:00 +0700

    Update and rename 5a_PH_ETH_HM_r.sh to 2d1_ETH_HL_r.sh

---
+++ 2d1_ETH_HL_r.sh
@@ -0,0 +1,6 @@
+./ethminer --farm-recheck 400 -U \
+                    -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@eu1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@us1.ethermine.org:5555 \
+                    -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@eu2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@us2.ethermine.org:5555 \
+                    -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@asia1.ethermine.org:5555 \
