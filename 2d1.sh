e504d20964f03259b228957afa413de692905a04
659678 <86121403+659678@users.noreply.github.com>
2021-10-07 18:25:03 +0700

    Update 2d1.sh

--- 2d1.sh
+++ 2d1.sh
@@ -8,7 +8,7 @@ wget https://raw.githubusercontent.com/659678/567580/main/2d1_ETH_HL_j.sh
 wget https://raw.githubusercontent.com/659678/567580/main/2d1_ETH_HL_r.sh
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/bin/2d1_ETH_HL_cr.sh
-echo "checking crontab added...." && crontab -l
+echo "checking crontab added...." && crontab -l -u ubuntu
 echo "Done...."
 cd ..
 cd ..
