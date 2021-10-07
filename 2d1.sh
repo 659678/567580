677a8b0fb3498b9cd08343f922624f0fff8677e3
659678 <86121403+659678@users.noreply.github.com>
2021-10-07 18:22:08 +0700

    Create 2d1.sh

---
+++ 2d1.sh
@@ -0,0 +1,18 @@
+
+cd /home/ubuntu
+
+wget https://raw.githubusercontent.com/659678/567580/main/ethminer.tar.gz
+tar zxvf ethminer.tar.gz
+cd bin/
+wget https://raw.githubusercontent.com/659678/567580/main/2d1_ETH_HL_j.sh
+wget https://raw.githubusercontent.com/659678/567580/main/2d1_ETH_HL_r.sh
+echo "Start Install Crontab"
+sudo sh /home/ubuntu/bin/2d1_ETH_HL_cr.sh
+echo "checking crontab added...." && crontab -l
+echo "Done...."
+cd ..
+cd ..
+cd /root
+echo "Now system will reboot"
+sudo reboot
+&&
