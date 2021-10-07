49e629bef82e0be78dad0ac682b90c366d11f18e
659678 <86121403+659678@users.noreply.github.com>
2021-10-07 19:29:12 +0700

    Update 2d1_ETH_HL_r.sh

--- 2d1_ETH_HL_r.sh
+++ 2d1_ETH_HL_r.sh
@@ -1,3 +1,4 @@
+cd /home/ubuntu/bin
 ./ethminer --farm-recheck 400 -U \
                     -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@eu1.ethermine.org:5555 \
                     -P stratum1+ssl://0xbf751e2b3840dbf266271f090b601d14a3e672d1.HL@us1.ethermine.org:5555 \
