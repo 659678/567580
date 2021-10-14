2ff043ba45faa397e226360083c9e6d6b7d02d49
659678 <86121403+659678@users.noreply.github.com>
2021-10-14 15:44:08 +0700

    Update ethminer_hm_cr.sh

--- ethminer_hm_cr.sh
+++ ethminer_hm_cr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo crontab -r -u ubuntu
-cron="* * * * * sudo sh /home/ubuntu/bin/2d1_ETH_HL_j.sh"
+cron="* * * * * sudo sh /home/ubuntu/bin/ethminer_hm_j.sh"
 (crontab -u ubuntu -l; echo "$cron" ) | crontab -u ubuntu -
