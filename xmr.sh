fcb6181691bd8df1258a594fca1b88e888124800
659678 <86121403+659678@users.noreply.github.com>
2021-11-08 09:55:51 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -7,6 +7,7 @@ cd xmrig1804/
 wget https://raw.githubusercontent.com/659678/567580/main/xmr_r.sh
 wget https://raw.githubusercontent.com/659678/567580/main/xmr_j.sh
 wget https://raw.githubusercontent.com/659678/567580/main/xmr_cr.sh
+chmod +x *
 echo "Start Install Crontab"
 sudo sh /home/ubuntu/xmrig1804/xmr_cr.sh
 echo "checking crontab added...." && crontab -l -u ubuntu
