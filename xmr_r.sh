5f4df771882f05752a835f8517bb45facc15dbaf
659678 <86121403+659678@users.noreply.github.com>
2021-11-10 09:36:33 +0700

    Update xmr_r.sh

--- xmr_r.sh
+++ xmr_r.sh
@@ -1,2 +1,7 @@
 cd /home/ubuntu/xmrig1804/
-./xmrig -o fr.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id PHAP -o ca.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id CANADA -o pool.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id EU -o sg.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id ASIA -o us-west.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id US
+./xmrig
+#POOL=rx.unmineable.com:3333
+#USER=XMR:8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb.h
+#./xmrig -o ca.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id CANADA
+#./xmrig -o rx.unmineable.com:3333 -u XMR:8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb.h -k --tls
+#./xmrig -o $POOL -a rx -k -u $USER -p x
