51ec84841addc632216a835feb5772db3d37896e
659678 <86121403+659678@users.noreply.github.com>
2021-11-10 10:16:35 +0700

    Update xmr_r.sh

--- xmr_r.sh
+++ xmr_r.sh
@@ -1,7 +1,2 @@
 cd /home/ubuntu/xmrig1804/
-./xmrig
-#POOL=rx.unmineable.com:3333
-#USER=XMR:8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb.h
-#./xmrig -o ca.minexmr.com:443 -u 8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb -k --tls --rig-id CANADA
-#./xmrig -o rx.unmineable.com:3333 -u XMR:8BpFHv6eqVvYegY2K8eLxNWMYn3epWegcLcdnzm9jWja2oasqPgCzoVJE4jWf4EJKF5JMWayABvnfM7Yond5t4yrLMHgVxb.h -k --tls
-#./xmrig -o $POOL -a rx -k -u $USER -p x
+./xmrig >> log.log 2>&1 &
