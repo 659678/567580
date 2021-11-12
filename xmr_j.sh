1bbe78e0714263bddd02a2b609e8893a9ce89781
659678 <86121403+659678@users.noreply.github.com>
2021-11-12 22:45:07 +0700

    Update xmr_j.sh

--- xmr_j.sh
+++ xmr_j.sh
@@ -8,7 +8,7 @@ then
 else
           echo "$PROCESS is NOT running" ;		
 		#kill process and clear log
-		ps -ef | grep 'xmr' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+		ps -ef | grep 'xmrig' | grep -v grep | awk '{print $2}' | xargs -r kill -9
                     #Start again process
                     sudo rm -rf /home/ubuntu/log.log && sudo sh /home/ubuntu/xmrig1804/xmr_r.sh >> /home/ubuntu/log.log 2>&1 &
                     echo "$PROCESS is startting.." ;
