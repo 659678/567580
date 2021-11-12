d4f9ec5fc44bb62595434cf1b98f1aedf1956e94
659678 <86121403+659678@users.noreply.github.com>
2021-11-12 22:41:41 +0700

    Update xmr_j.sh

--- xmr_j.sh
+++ xmr_j.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-STATUS=$(/bin/ps ax | grep -w "xmr" | grep -v grep)
+PROCESS="xmr";
 
-if [ "$STATUS" != "" ]
+if ps ax | grep -v grep | grep $PROCESS > /dev/null
 then
-          echo "xmr_running"
-            exit 0
-    else
-              echo "xmr_stop"
-                                sudo rm -rf /home/ubuntu/log.log && sudo sh /home/ubuntu/xmrig1804/xmr_r.sh >> /home/ubuntu/log.log 2>&1 &
-                                  echo "xmr_started"
-
-                          fi
+        echo "$PROCESS is running" ;
+		#nothing todo
+else
+          echo "$PROCESS is NOT running" ;		
+		#kill process and clear log
+		ps -ef | grep 'xmr' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+                    #Start again process
+                    sudo rm -rf /home/ubuntu/log.log && sudo sh /home/ubuntu/xmrig1804/xmr_r.sh >> /home/ubuntu/log.log 2>&1 &
+                    echo "$PROCESS is startting.." ;
+fi
