fcd82d17118d13095777a7d78f329d9d88225321
659678 <86121403+659678@users.noreply.github.com>
2021-11-12 23:18:46 +0700

    Update 2d1_ETH_HL_j.sh

--- 2d1_ETH_HL_j.sh
+++ 2d1_ETH_HL_j.sh
@@ -10,6 +10,6 @@ else
         echo "$PROCESS is NOT running" ;		
 		ps -ef | grep 'etherminer' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 		sudo su -
-		sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/2d1_ETH_HL_r.sh >> /home/ubuntu/log.log 2>&1 &
+		cd /home/ubuntu/ && rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/2d1_ETH_HL_r.sh >> /home/ubuntu/log.log 2>&1 &
 		echo "$PROCESS is startting.." ;
 fi
