bec6de2922995a14dc51aba4cededaf119aa1c7b
659678 <86121403+659678@users.noreply.github.com>
2021-11-15 09:17:31 +0700

    Update 2d1_ETH_HL_j.sh

--- 2d1_ETH_HL_j.sh
+++ 2d1_ETH_HL_j.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 PROCESS="ethminer";
+STATUS=$(/bin/ps ax | grep -w "$PROCESS" | grep -v grep)
 
-if ps ax | grep -v grep | grep $PROCESS > /dev/null
+if [ "$STATUS" != "" ]
 then
         echo "$PROCESS is running" ;
-		
+                #nothing todo
 else
-        echo "$PROCESS is NOT running" ;		
-		ps -ef | grep 'etherminer' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+          echo "$PROCESS is NOT running" ;
+                #kill process and clear log
+        	#sudo ps -ef | grep 'ethminer' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 		sudo su -
 		cd /home/ubuntu/bin/ && sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/2d1_ETH_HL_r.sh >> /home/ubuntu/log.log 2>&1 &
 		echo "$PROCESS is startting.." ;
